952a2cc8f6a0cc04b7ca5344bef539eb1f3a7380
Chaejongin12 <s22055@gsm.hs.kr>
2023-09-07 15:09:28 +0900

    :memo: :: LocalAuthDataSourceImpl 작성
    :: LocalAuthDataSourceImpl 작성

--- data/src/main/java/com/goms/data/local/datasource/LocalAuthDataSourceImpl.kt
+++ data/src/main/java/com/goms/data/local/datasource/LocalAuthDataSourceImpl.kt
@@ -1,10 +1,12 @@
 package com.goms.data.local.datasource
 
 import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
 import com.goms.data.local.key.AuthPreferenceKey
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
-import java.util.prefs.Preferences
+
 import javax.inject.Inject
 
 class LocalAuthDataSourceImpl @Inject constructor(
@@ -15,7 +17,6 @@ class LocalAuthDataSourceImpl @Inject constructor(
         it[AuthPreferenceKey.ACCESS_TOKEN] ?: ""
     }
 
-
     override suspend fun setAccessToken(accessToken: String) {
         dataStore.edit{
             it[AuthPreferenceKey.ACCESS_TOKEN] = accessToken
@@ -23,54 +24,56 @@ class LocalAuthDataSourceImpl @Inject constructor(
     }
 
     override suspend fun removeAccessToken() {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun getRefreshToken(): String? =
-        localStorage.getRefreshToken()
-
-    override suspend fun setRefreshToken(refreshToken: String) {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun removeRefreshToken() {
-        TODO("Not yet implemented")
-    }
-
-
-    override suspend fun saveToken(
-        access: String?,
-        refresh: String?,
-        refreshTokenExpiredAt: String?,
-        accessTokenExpiredAt: String?
-    ) =
-        localStorage.run {
-            setAccessToken(access)
-            setRefreshToken(refresh)
-            setRefreshTokenExpiredAt(refreshTokenExpiredAt)
-            setAccessTokenExpiredAt(accessTokenExpiredAt)
+        dataStore.edit {
+            it.remove(AuthPreferenceKey.ACCESS_TOKEN)
         }
+    }
 
-    override suspend fun getAccessTokenExpiredAt(): String? =
-        localStorage.getAccessTokenExpiredAt()
+    override suspend fun getAccessTokenExpiredAt(): Flow<String> = dataStore.data.map {
+        it[AuthPreferenceKey.ACCESS_TOKEN_EXPIRED_AT] ?: ""
+    }
 
     override suspend fun setAccessTokenExpiredAt(accessTokenExpiredAt: String) {
-        TODO("Not yet implemented")
+        dataStore.edit {
+            it[AuthPreferenceKey.ACCESS_TOKEN_EXPIRED_AT] = accessTokenExpiredAt
+        }
     }
 
     override suspend fun removeAccessExpiredAt() {
-        TODO("Not yet implemented")
+        dataStore.edit {
+            it.remove(AuthPreferenceKey.ACCESS_TOKEN_EXPIRED_AT)
+        }
     }
 
-    override suspend fun getRefreshTokenExpiredAt(): String? =
-        localStorage.getRefreshTokenExpiredAt()
+    override suspend fun getRefreshToken(): Flow<String> = dataStore.data.map {
+        it[AuthPreferenceKey.REFRESH_TOKEN] ?: ""
+    }
+
+    override suspend fun setRefreshToken(refreshToken: String) {
+        dataStore.edit {
+            it[AuthPreferenceKey.REFRESH_TOKEN] = refreshToken
+        }
+    }
+
+    override suspend fun removeRefreshToken() {
+        dataStore.edit {
+            it.remove(AuthPreferenceKey.REFRESH_TOKEN)
+        }
+    }
+    override suspend fun getRefreshTokenExpiredAt(): Flow<String> = dataStore.data.map {
+        it[AuthPreferenceKey.REFRESH_TOKEN_EXPIRED_AT] ?: ""
+    }
 
     override suspend fun setRefreshTokenExpiredAt(refreshTokenExpiredAt: String) {
-        TODO("Not yet implemented")
+        dataStore.edit {
+            it[AuthPreferenceKey.REFRESH_TOKEN_EXPIRED_AT] = refreshTokenExpiredAt
+        }
     }
 
     override suspend fun removeRefreshExpiredAt() {
-        TODO("Not yet implemented")
+        dataStore.edit {
+            it.remove(AuthPreferenceKey.REFRESH_TOKEN_EXPIRED_AT)
+        }
     }
 
 }
