8b7177ef57bce108338d535e0f607b6a974afc27
박성현 <pjhomg5530@naver.com>
2023-09-12 21:10:57 +0900

    :lipstick: :: color 임시로 추가

--- presentation/src/main/java/com/example/goms_android_v2_admin/ui/theme/Color.kt
+++ presentation/src/main/java/com/example/goms_android_v2_admin/ui/theme/Color.kt
@@ -9,3 +9,10 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Background = Color(0xFFF7F7F9)
+
+val Primary = Color(0xFF5F9DF7)
+
+val n1 = Color(0xFF959595)
+val n2 = Color(0xFF787878)
